d0fe5da96df051983102754e9400e3d000af144b
SimoniShah <shahsimoni@yahoo.com>
2015-04-22 16:20:44 +0530

    This is the modified cachematrix.R file for the Programming assignment 2, R programming course, coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions are used to create a special matrix object 
+## that may be used to manage and cache the inverse, 
+## so that it is computed only once.
 
-## Write a short comment describing this function
+## The function makeCacheMatrix takes a matrix object as an argument 
+## and returns a list of functions that are used to set ang get
+## the matrix itself, as well as its inverse (special matrix object)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invmat=NULL
+  
+  setMat<-function(m)
+  {
+    x<<-m
+    invmat<<-NULL   #invmat is the inverse matrix.
+  }
+  getMat<- function()
+  {
+    x
+  }
+  setInv<- function(m)
+  {
+    invmat<<-m
+  }
+  getInv<- function()
+  {
+    invmat
+  }
+  list(setMat=setMat,getMat=getMat,setInv=setInv,getInv=getInv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function which takes the special matrix object 
+## (that is returned by the makeCacheMatrix function) 
+## and returns the inverse matrix. 
+## If the inverse is qureyed for the first time, it is computed and cached.
+## For future queries, the inverse is retrived from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  tempObj <- x$getInv()
+  if(is.null(tempObj))
+  {
+    print("Inverse not computed yet")
+    tempInv=solve(x$getMat())
+    x$setInv(tempInv)  
+  }
+  else
+  {
+    print("Inverse already computed")
+  }
+  x$getInv()
 }
